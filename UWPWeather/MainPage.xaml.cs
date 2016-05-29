c2eb522c58fb8a0c0fd98b462030a77830477568
Giovan <Giovan@CARBONVECTOR02>
2016-05-29 16:10:12 +0700

    UI Finishing

--- UWPWeather/MainPage.xaml.cs
+++ UWPWeather/MainPage.xaml.cs
@@ -31,16 +31,22 @@ namespace UWPWeather
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             var position = await LocationManager.GetPosition();
-            
+
             RootObject myWeather = await OpenWeatherMapProxy.GetWeather(
                 position.Coordinate.Latitude,
                 position.Coordinate.Longitude);
-            
-            ResultTextBlock.Text = $"{myWeather.name} - {myWeather.main.temp} K - {myWeather.weather[0].description}";
+
+            //ResultTextBlock.Text = $"{myWeather.name} - {myWeather.main.temp} K - {myWeather.weather[0].description}";
             //string icon = $"http://openweathermap.org/img/w/{myWeather.weather[0].icon}.png";
             //ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
             //ResultImage.Source = new BitmapImage(new Uri($"http://openweathermap.org/img/w/{myWeather.weather[0].icon}.png", UriKind.Absolute));
             ResultImage.Source = new BitmapImage(new Uri($"ms-appx:///Assets/Weather/{myWeather.weather[0].icon}.png", UriKind.Absolute));
+            WeatherTextBlock.Text = myWeather.weather[0].description;
+            MapAsset.Text = ((char)0xE707).ToString();
+            Place.Text = $"{myWeather.name},";
+            TemperatureTextBlock.Text = $"{(myWeather.main.temp - 273).ToString()} {(char)0x00B0}C";
+
+            GetWeatherButton.Visibility = Visibility.Collapsed;
         }
     }
 }
